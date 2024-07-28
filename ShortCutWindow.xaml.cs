a6dd1428d49eb1dec3daf6690bc846a19e307ddc
Lioda9130 <Lioda9130@gmail.com>
2024-07-28 14:28:16 +0900

    41차 커밋 [언어 선택별 열리는 창 추가]

--- ShortCutWindow.xaml.cs
+++ ShortCutWindow.xaml.cs
@@ -7,11 +7,11 @@ namespace Hcode
 {
     public partial class ShortCutWindow : Window
     {
-        //인자로 받아온 윈도우와 선택된 언어
+        // 인자로 받아온 윈도우와 선택된 언어
         private Window window;
         private string selectLanguage;
 
-        //Hcode 폴더 경로 불러오기
+        // Hcode 폴더 경로 불러오기
         static string folderPath = "C:/Hcode";
         static string projectPath = folderPath + "/Project";
 
@@ -26,20 +26,53 @@ namespace Hcode
 
         private void okButton_Click(object sender, RoutedEventArgs e)
         {
-            //프로젝트명 가져오기
+            // 프로젝트명 가져오기
             string fileName = FileName_TextBox.Text;
             DirectoryInfo newProjectInfoPath = new DirectoryInfo(projectPath + "/" + fileName);
-            FileInfo newFilePath = new FileInfo(projectPath + "/" + fileName + "/" + fileName + ".c");
+            FileInfo newFilePath;
 
-            //해당 프로젝트 폴더 유무 체크 후 없을시 생성
+            // 선택된 언어에 따른 파일 확장자 설정
+            switch (selectLanguage.ToLower())
+            {
+                case "c":
+                    newFilePath = new FileInfo(projectPath + "/" + fileName + "/" + fileName + ".c");
+                    break;
+                case "java":
+                    newFilePath = new FileInfo(projectPath + "/" + fileName + "/" + fileName + ".java");
+                    break;
+                case "python":
+                    newFilePath = new FileInfo(projectPath + "/" + fileName + "/" + fileName + ".py");
+                    break;
+                default:
+                    MessageBox.Show("지원되지 않는 언어입니다.");
+                    return;
+            }
+
+            // 해당 프로젝트 폴더 유무 체크 후 없을 시 생성
             if (!newProjectInfoPath.Exists)
             {
                 newProjectInfoPath.Create();
                 newFilePath.Create();
             }
 
-            //Window를 연다. !! 지금은 CWindow를 객체로 생성하는데 Java와 Python을 구현할땐 바꿔야한다 !!
-            Window newWindow = new CWindow(selectLanguage, FileName_TextBox.Text);
+            // 선택된 언어에 따른 Window 열기
+            Window newWindow;
+            switch (selectLanguage.ToLower())
+            {
+                case "c":
+                    newWindow = new CWindow(selectLanguage, fileName);
+                    break;
+                case "java":
+                    newWindow = new JavaWindow(selectLanguage, fileName);
+                    break;
+                case "python":
+                    newWindow = new PythonWindow(selectLanguage, fileName);
+                    break;
+                default:
+                    MessageBox.Show("지원되지 않는 언어입니다.");
+                    return;
+            }
+
             newWindow.Show();
             window.Close();
             this.Close();
